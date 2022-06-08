1d3df4070300c4b4d63596e585a84afb59441228
zetta shao <zphalanx@gmail.com>
2022-06-08 03:05:44 -0400

    first release

---
+++ mkmod.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+TGTD="vmware-host-modules"
+BRANCH="workstation-12.5.9"
+if ! [ -z "${1}" ]; then BRANCH=${1}; fi
+if [ "${1}" = "clean" ]; then
+rm -rf /usr/src/vmware-host-modules-*
+rm -rf /var/lib/dkms/vmware-host-modules
+rm -rf ${TGTD}
+exit
+fi
+echo "branch is: "${BRANCH}
+if ! [ -d "${TGTD}" ]; then
+git clone https://github.com/mkubecek/vmware-host-modules.git -b ${BRANCH} ${TGTD}
+fi
+cp -f dkms_Makefile ${TGTD}/
+cp -f vmmon_Makefile ${TGTD}/vmmon-only/dkms_Makefile
+cp -f vmnet_Makefile ${TGTD}/vmnet-only/dkms_Makefile
+make dkms --file=dkms_Makefile -C ${TGTD}
