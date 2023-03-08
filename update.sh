62bc8cba9927607e9f96c3007af3ca3a66709210
zetta-shao <80302378+zetta-shao@users.noreply.github.com>
2023-03-08 09:47:01 +0800

    Update update.sh
    remove old source code.

--- update.sh
+++ update.sh
@@ -2,8 +2,11 @@
 TGTM=`dkms status vmware-host-modules|cut -f1 -d','`
 if ! [ -z "${TGTM}" ]; then
 echo ${TGTM}
-dkms remove ${TGTM}
+TGTS=`echo ${TGTM}|sed 's/\//-/g'`
 dkms uninstall ${TGTM}
+dkms remove ${TGTM}
+rm -rf /usr/src/${TGTS}
 fi
+./mkmod.sh
 cd vmware-host-modules
 make dkms --file=dkms_Makefile
