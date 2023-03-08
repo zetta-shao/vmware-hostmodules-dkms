324a2f6e938ab478b9704aa1ba46ef7b391599ef
zetta-shao <80302378+zetta-shao@users.noreply.github.com>
2023-03-08 10:45:55 +0800

    update clean-module step
    update clean-module step

--- update.sh
+++ update.sh
@@ -1,12 +1,4 @@
 #!/bin/bash
 TGTM=`dkms status vmware-host-modules|cut -f1 -d','`
-if ! [ -z "${TGTM}" ]; then
-echo ${TGTM}
-TGTS=`echo ${TGTM}|sed 's/\//-/g'`
-dkms uninstall ${TGTM}
-dkms remove ${TGTM}
-rm -rf /usr/src/${TGTS}
-fi
+if ! [ -z "${TGTM}" ]; then ./mkmod.sh clean; fi
 ./mkmod.sh
-cd vmware-host-modules
-make dkms --file=dkms_Makefile
