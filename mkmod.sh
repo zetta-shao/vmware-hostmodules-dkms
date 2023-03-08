807bcc7870409cfefbf1f551719e7fd3ccd8eb8f
zetta-shao <80302378+zetta-shao@users.noreply.github.com>
2023-03-08 10:45:22 +0800

    update clean-module step
    update clean-module step

--- mkmod.sh
+++ mkmod.sh
@@ -3,15 +3,15 @@ TGTD="vmware-host-modules"
 BRANCH="workstation-12.5.9"
 if ! [ -z "${1}" ]; then BRANCH=${1}; fi
 if [ "${1}" = "clean" ]; then
-rm -rf /usr/src/vmware-host-modules-*
-rm -rf /var/lib/dkms/vmware-host-modules
-rm -rf ${TGTD}
-rmmod vmnet
-rmmod vmmon
-rm -f /lib/modules/*/updates/dkms/vmmon.ko /lib/modules/*/updates/dkms/vmnet.ko
-depmod -a
-exit
-fi
+  TGTM=`dkms status vmware-host-modules|cut -f1 -d','`
+  if ! [ -z "${TGTM}" ]; then
+    systemctl stop vmware
+    TGTS=`echo ${TGTM}|sed 's/\//-/g'`
+    dkms uninstall ${TGTM}
+    dkms remove ${TGTM}
+    modprobe -r vmnet vmmon
+    rm -rf /usr/src/${TGTS}
+    depmod -a; exit; fi; fi
 echo "branch is: "${BRANCH}
 if ! [ -d "${TGTD}" ]; then
 git clone https://github.com/mkubecek/vmware-host-modules.git -b ${BRANCH} ${TGTD}
