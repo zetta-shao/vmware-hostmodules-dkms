a1d4c4761b269326702859ce098872d4227f0257
zetta shao <zphalanx@gmail.com>
2024-05-27 14:06:39 +0800

    fixed sudo issue

--- mkmod.sh
+++ mkmod.sh
@@ -3,15 +3,15 @@ TGTD="vmware-host-modules"
 BRANCH="workstation-12.5.9"
 if ! [ -z "${1}" ]; then BRANCH=${1}; fi
 if [ "${1}" = "clean" ]; then
-  TGTM=`dkms status vmware-host-modules|cut -f1 -d','`
+  TGTM=`dkms status vmware-host-modules|awk -F'[,|:]' '{print $1}'`
   if ! [ -z "${TGTM}" ]; then
-    systemctl stop vmware
+    sudo systemctl stop vmware
     TGTS=`echo ${TGTM}|sed 's/\//-/g'`
-    dkms uninstall ${TGTM}
-    dkms remove ${TGTM}
-    modprobe -r vmnet vmmon
-    rm -rf /usr/src/${TGTS}
-    depmod -a; exit; fi; exit; fi
+    sudo dkms uninstall ${TGTM}
+    sudo dkms remove ${TGTM}
+    sudo modprobe -r vmnet vmmon
+    sudo rm -rf /usr/src/${TGTS}
+    sudo depmod -a; exit; fi; exit; fi
 echo "branch is: "${BRANCH}
 if ! [ -d "${TGTD}" ]; then
 git clone https://github.com/mkubecek/vmware-host-modules.git -b ${BRANCH} ${TGTD}
@@ -19,4 +19,4 @@ fi
 cp -f dkms_Makefile ${TGTD}/
 cp -f vmmon_Makefile ${TGTD}/vmmon-only/dkms_Makefile
 cp -f vmnet_Makefile ${TGTD}/vmnet-only/dkms_Makefile
-make dkms --file=dkms_Makefile -C ${TGTD}
+sudo make dkms --file=dkms_Makefile -C ${TGTD}
