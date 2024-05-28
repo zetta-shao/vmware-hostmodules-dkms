89cb540f27275115a462c7198d8cbbfe05aa4de3
zetta shao <zphalanx@gmail.com>
2024-05-28 12:23:05 +0800

    bit fix for 17.5.1

--- mkmod.sh
+++ mkmod.sh
@@ -9,7 +9,8 @@ if [ "${1}" = "clean" ]; then
     TGTS=`echo ${TGTM}|sed 's/\//-/g'`
     sudo dkms uninstall ${TGTM}
     sudo dkms remove ${TGTM}
-    sudo modprobe -r vmnet vmmon
+    sudo modprobe -r vmnet
+    sudo modprobe -r vmmon
     sudo rm -rf /usr/src/${TGTS}
     sudo depmod -a; exit; fi; exit; fi
 echo "branch is: "${BRANCH}
@@ -19,4 +20,7 @@ fi
 cp -f dkms_Makefile ${TGTD}/
 cp -f vmmon_Makefile ${TGTD}/vmmon-only/dkms_Makefile
 cp -f vmnet_Makefile ${TGTD}/vmnet-only/dkms_Makefile
-sudo make dkms --file=dkms_Makefile -C ${TGTD}
+cd ${TGTD}
+git apply ../0001-fix-define.patch
+cd ..
+sudo make dkms --file=dkms_Makefile KVER=$(uname -r) -C ${TGTD}
