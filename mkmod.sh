09771d5f5c6a574c7299437dbc82ea21eb980f15
zetta shao <zphalanx@gmail.com>
2024-06-22 14:40:33 +0800

    add vmware service control

--- mkmod.sh
+++ mkmod.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 TGTD="vmware-host-modules"
-BRANCH="workstation-12.5.9"
+BRANCH="workstation-17.0.2"
+VMCTL="/etc/init.d/vmware"
 if ! [ -z "${1}" ]; then BRANCH=${1}; fi
-sudo /etc/init.d/vmware stop
+if [ -r ${VMCTL} ]; then sudo ${VMCTL} stop; fi
 if [ "${1}" = "clean" ]; then
   TGTM=`dkms status|grep vmware-host-modules|awk -F'[,|:]' '{print $1}'`
   if ! [ -z "${TGTM}" ]; then
     sudo systemctl stop vmware
     TGTS=`echo ${TGTM}|sed 's/\//-/g'`
-    sudo dkms uninstall ${TGTM}
     sudo make dkms_clean --file=dkms_Makefile -C ${TGTD}
+    sudo dkms uninstall ${TGTM}
     sudo rm -rf /usr/src/${TGTS}
     sudo depmod -a;
     if [ -d '/var/lib/dkms/vmware-host-modules' ]; then 
@@ -21,11 +22,9 @@ if ! [ -d "${TGTD}" ]; then
   git clone https://github.com/mkubecek/vmware-host-modules.git -b ${BRANCH} ${TGTD}
   git config --global --add safe.directory $(pwd)/${TGTD}
 fi
-cp -f dkms_Makefile ${TGTD}/
-cp -f vmmon_Makefile ${TGTD}/vmmon-only/dkms_Makefile
-cp -f vmnet_Makefile ${TGTD}/vmnet-only/dkms_Makefile
 cd ${TGTD}
 git reset $(git log|head -1|awk '{print $2}') --hard
+git checkout master
 git checkout ${BRANCH}
 VER=$(git describe --long --always|awk -F'-' '{print $1}')
 if [ ${VER} = "w17.5.1" ]; then
@@ -38,5 +37,8 @@ elif [ ${VER} = "w12.5.9" ]; then
   touch patched
 fi
 cd ..
+cp -f dkms_Makefile ${TGTD}/
+cp -f vmmon_Makefile ${TGTD}/vmmon-only/dkms_Makefile
+cp -f vmnet_Makefile ${TGTD}/vmnet-only/dkms_Makefile
 sudo make dkms --file=dkms_Makefile -C ${TGTD}
-sudo /etc/init.d/vmware start
+if [ -r ${VMCTL} ]; then sudo ${VMCTL} start; fi
